0a93a0b226cb1c8d20c9d6ee86fc6cc173cfdd00
ujwalpandey012 <ujwalpandey012@gmail.com>
2025-11-26 22:43:02 +1100

    Update script.js

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,24 +1,6 @@
-// SCROLL REVEAL
-const revealElements = document.querySelectorAll(
-    ".fade-in, .slide-up, .service-card, .pricing-card, .project-card, .page-section"
-);
-
-function reveal() {
-    revealElements.forEach(el => {
-        const top = el.getBoundingClientRect().top;
-        if (top < window.innerHeight - 70) {
-            el.classList.add("show");
-        }
-    });
-}
-
-window.addEventListener("scroll", reveal);
-reveal();
-
-// MOBILE MENU
-const menuToggle = document.getElementById("menuToggle");
+const menuBtn = document.getElementById("menuBtn");
 const navMenu = document.getElementById("navMenu");
 
-menuToggle.addEventListener("click", () => {
+menuBtn.addEventListener("click", () => {
     navMenu.classList.toggle("open");
 });
